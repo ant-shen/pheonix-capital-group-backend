887b70297fe93bc81cb67d59bb44252bf5a331e6
ant-shen <104231150+ant-shen@users.noreply.github.com>
2024-09-12 16:33:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+
+app.use(cors({
+    origin: 'http://localhost:8080', 
+    //origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true 
+  }));
+  
+app.use(express.json());
+
+// Routing
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/login', require('./routes/loginRoute'));
+app.use('/api/owners', require('./routes/owners'));
+app.use('/api/landholdings', require('./routes/landHoldings'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
