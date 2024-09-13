00e561275660b7069283a5cdb280c5f0f181ac27
ant-shen <anthony.shen11@gmail.com>
2024-09-12 17:45:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
     //origin: 'http://localhost:8080', 
-    origin: 'https://phoenix-capital-group-deployed.onrender.com/',
+    origin: 'https://phoenix-capital-group-deployed.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
